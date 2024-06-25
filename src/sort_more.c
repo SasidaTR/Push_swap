6610cd3f3f2f91797c10d4836d2ee4f964b7e10f
Tommy Rischmann <trischma@made-f0Cr7s2.clusters.42paris.fr>
2024-06-25 18:11:57 +0200

    Push automatique :)

--- src/sort_more.c
+++ src/sort_more.c
@@ -6,7 +6,7 @@
 /*   By: trischma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 12:38:27 by trischma          #+#    #+#             */
-/*   Updated: 2024/06/25 17:47:30 by trischma         ###   ########.fr       */
+/*   Updated: 2024/06/25 18:11:46 by trischma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,36 +47,48 @@ void	push_min_from_a(t_stack *a, t_stack *b, t_pos *pos)
 		}
 	}
 	pb(a, b);
-	if (b->size > 1 && b->arr[0] > b->arr[1])
-		sb(b);
 }
 
-	t_pos	find_value(t_stack *a, t_stack *b)
-	{
-		t_pos	pos;
-		int		i;
-		int		j;
+t_pos	find_value(t_stack *a, t_stack *b)
+{
+	t_pos	pos;
+	int		i;
+	int		j;
 
-		pos.posA = 0;
-		pos.posB = 0;
-		i = 0;
-		while (i < a->size)
+	pos.posA = 0;
+	pos.posB = 0;
+	i = 0;
+	while (i < a->size)
+	{
+		j = 0;
+		while (j < b->size)
 		{
-			j = 0;
-			while (j < b->size)
+			if (a->arr[i] - b->arr[j] < a->arr[pos.posA] - b->arr[pos.posB])
 			{
-				if (a->arr[i] < a->arr[pos.posA] || 
-					(a->arr[i] == a->arr[pos.posA] && b->arr[j] < b->arr[pos.posB]))
-				{
-					pos.posA = i;
-					pos.posB = j;
-				}
-				j++;
+				pos.posA = i;
+				pos.posB = j;
 			}
-			i++;
+			j++;
 		}
-		return (pos);
+		i++;
 	}
+	i = a->size - 5;
+	while (i < a->size && i >= 0)
+	{
+		j = 0;
+		while (j < b->size)
+		{
+			if (a->arr[i] - b->arr[j] < a->arr[pos.posA] - b->arr[pos.posB])
+			{
+				pos.posA = i;
+				pos.posB = j;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (pos);
+}
 
 void	organize_more(t_stack *a, t_stack *b)
 {
@@ -84,7 +96,7 @@ void	organize_more(t_stack *a, t_stack *b)
 	pb(a, b);
 	pb(a, b);
 	if (b->arr[0] < b->arr[1])
-		sb(b);
+		rb(b);
 	while (a->size > 0)
 	{
 		pos = find_value(a, b);
